f49fec52ce34e2f87214b08f9c587ee8c0489fdd
Korli <korlilarryaddo43@gmail.com>
2025-01-09 22:40:48 +0000

    SGD

--- SGD-1.py
+++ SGD-1.py
@@ -4,10 +4,11 @@ from nnfs.datasets import spiral_data
 from Backpropagation import DenseLayer,softmax,activationRelu,loss,categorical_CrossEntropyLoss,Activation_Softmax_Loss_CategoricalCrossEntopy
 
 
+
 # SGD optimizer
 class Optimizer_SGD:
-# Initialize optimizer - set settings,
-# learning rate of 1. is default for this optimizer
+    # Initialize optimizer - set settings,
+    # learning rate of 1. is default for this optimizer
 
     def __init__(self, learning_rate=1., decay=0.):
         self.learning_rate = learning_rate
